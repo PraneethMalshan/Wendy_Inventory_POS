b753e241fee2a2622716c737cfe4380d9287c792
PraneethMalshan <praneethmalshan12345@gmail.com>
2024-03-01 19:29:13 +0530

    Added dashboard's cards

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,91 @@
 import React from "react";
 import Sidenav from "../component/Sidenav";
+import NavBar from "../component/NavBar";
 import Box from '@mui/material/Box';
-import Typography from "@mui/material/Typography";
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Stack from '@mui/material/Stack';
+import "../Dash.css";
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+
 
 export default function Dashboard() {
     return (
         <>
-            <Box sx={{ display: 'flex' }}>
-                <Sidenav/>
+            <div className="bgColor">
 
-                <Box component="main" sx={{flexGrow: 1, p: 3}}>
-                    <h1>Dashboard</h1>
+                <NavBar />
+                <Box height={70}/>
+                <Box sx={{ display: 'flex'}}>
+                    <Sidenav/>
+                    <Box component="main" sx={{flexGrow: 1, p: 3}}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Stack spacing={2} direction="row" sx={{display: 'flex', alignItems:'center', justifyContent: 'center'}}>
+                                    {/*<Card sx={{ maxWidth: 345 }}>*/}
+                                    <Card sx={{ minWidth: 30 + "%", height: 140 }}>
+
+                                        <CardContent>
+                                            <Stack spacing={2} direction="row">
+                                                <div className="iconStyle">
+                                                    <MonetizationOnIcon/>
+                                                </div>
+
+                                                <div className="paddingall">
+                                                    <span className="priceTitle">$4385</span>
+                                                    <br/>
+                                                    <span className="priceSubTitle">Total Inventory Value</span>
+                                                </div>
+                                            </Stack>
+                                        </CardContent>
+
+                                    </Card>
+                                    {/*<Card sx={{ maxWidth: 345 }}>*/}
+                                    <Card sx={{ minWidth: 30 + "%", height: 140 }}>
+
+                                        <CardContent>
+                                            <Stack spacing={2} direction="row">
+                                                <div className="iconStyle">
+                                                    <MonetizationOnIcon/>
+                                                </div>
+
+                                                <div className="paddingall">
+                                                    <span className="priceTitle">$4385</span>
+                                                    <br/>
+                                                    <span className="priceSubTitle">Total Inventory Value</span>
+                                                </div>
+                                            </Stack>
+                                        </CardContent>
+
+                                    </Card>
+                                </Stack>
+                            </Grid>
+                            {/*<Grid item xs={4}>*/}
+
+                            {/*</Grid>*/}
+                        </Grid>
+
+                        <Box height={20}/>
+
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Card sx={{ height: 60 + "vh" }}>
+
+                                    <CardContent>
+
+                                    </CardContent>
+
+                                </Card>
+                            </Grid>
+                            <Grid item xs={4}>
+
+                            </Grid>
+                        </Grid>
+                    </Box>
                 </Box>
-            </Box>
 
+            </div>
         </>
     )
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Sidenav from "../component/Sidenav";
 import Box from '@mui/material/Box';
+import NavBar from "../component/NavBar";
 import Typography from "@mui/material/Typography";
 
 export default function Product() {
     return (
         <>
+            <NavBar />
+            <Box height={30}/>
             <Box sx={{ display: 'flex' }}>
                 <Sidenav/>
-
                 <Box component="main" sx={{flexGrow: 1, p: 3}}>
                     <h1>Product</h1>
                 </Box>
